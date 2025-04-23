c77e9881d2b5839f198e0b84fcb84e6bd3096be5
kendysxsiden <kirillvrublevskiy@gmail.com>
2025-04-23 16:57:56 +0200

    ad/fix_api

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import TaxiSimulator from './components/TaxiSimulator';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <TaxiSimulator />
+    </ThemeProvider>
+  );
+}
